c2ee8210b5a64f7fbc9fbffeeff03d65bad9c0fb
Hamza <hamzakhangd77@gmail.com>
2026-09-06 19:39:28 +0530

    Pushing all files

---
+++ JS/Second.js
@@ -0,0 +1,101 @@
+// This is Operators
+// let a = 10;
+// let b= 20;
+// console.log("a = " , a ,"b =" ,b);
+// console.log("a + b=", a +  b);
+// console.log("a - b=", a -  b);
+// console.log("a * b=", a * b);
+// console.log("a / b=", a /  b);
+// console.log("a ** b=", a **  b);
+// console.log("a % b=", a %  b);
+
+
+// unary operator
+// let a = 10;
+// let b= 20;
+// console.log("a = " , a ,"b =" ,b);
+// a = ++a;
+// console.log("++a =" ,a );
+
+// Assignment operators
+// let a = 5;
+// let b = 2;
+
+// a **= 4;
+// console.log("a +=", a);
+// console.log("a -=", a);
+// console.log("a +=", a);
+// console.log("a /=", a);
+// console.log("a **=", a);
+
+// Comparison Operators
+
+// let a = 5;
+// let b = 4;
+// // console.log("a == b", a == b);
+// console.log("a < b" , a < b);
+// console.log("a > b" , a > b);
+// console.log("a <= b" , a <= b);
+// console.log("a >= b" , a >= b);
+
+ //logical Operators
+
+// let cond1 = true;
+// let cond2 = true;
+// console.log("cond1 && cond2", cond1 && cond2);
+// console.log("cond1 || cond2", cond1 || cond2);
+// console.log("!cond1", !cond1);
+
+//Conditional statement
+
+// let age = 16;
+// if (  age >= 18 ){
+//     console.log("You can vote");
+
+// } else  ( age < 18);{
+//     console.log("You cannot vote");
+
+// };
+
+// let mode = "silver";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode === "blue") {
+//     color = "blue";
+// } else if (mode === "red") {
+//     color = "red";
+// } else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// ternary operator
+
+// let age = 25;
+// let vote = ( age >= 18) ? "You can vote" : "You cannot vote";
+// console.log(vote);
+
+// alert("hello!")
+
+// let num = prompt("Enter a Number");
+// if (num % 3 === 0){
+//     console.log(num,"is a divible of 3 ")
+// }else{
+//     console.log(num,"is not a divible of 3 ")
+// }
+
+let marks = prompt("Enter your marks");
+if (marks >= 80 && marks <= 100){
+    console.log("A");
+}else if(marks >= 70 && marks <= 89){
+    console.log("B");
+}else if(marks >= 60 && marks <=69){
+    console.log("C");
+}else if(marks >= 50 && marks <=59){
+    console.log("D");
+}else {
+    console.log("F");
+}
