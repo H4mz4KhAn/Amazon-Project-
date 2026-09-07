9bf31bf4c8feb194d0267e72a07a77b371fed0cd
Hamza <hamzakhangd77@gmail.com>
2026-09-07 22:30:15 +0530

    Updated , new js

---
+++ JS/third.js
@@ -0,0 +1,64 @@
+// for (let i = 1; i <= 100000; i++){
+//     console.log("Apna college");
+// 
+
+// let sum = 0;
+// let n = 100;
+// for (i =0; i<=n; i++){
+//    sum = sum + i;
+// }
+// console.log("sum of first 100 natural numbers is", sum);
+
+
+// for (i = 1; i <= 10; i++){
+//  console.log("i", i);
+// }
+
+// let i = 1;
+// while (i <= 5){
+//     console.log("i =", i);
+//     i++;
+// }
+
+// let i = 20;
+// do{
+//     console.log("I = ",i);
+//     i++;
+// }while (i <=10);
+
+// let str = "javascript";
+// let sum = 0;
+// for (let i of str){
+//     console.log("i=", i);   <--- Loops on datatypes (string)
+//     sum++;
+// }
+// console.log("length of string is", sum);
+
+// let student ={
+//     name: "Hamza",
+//     age : 20,
+//     cgpa : 3.5,
+//     ispassed : true
+// };
+// for (let key in student){
+//     console.log("key = ", key, "value = ", student[key]);  
+// }
+
+// let i = 0;
+// for (i = 0; i <=100; i ++){
+//  if (i % 2 === 0){
+// console.log("i = ", i);
+// }
+// }
+
+// let gameNum = 25;
+// let userNum = prompt("Guess the number ");
+// while (userNum != gameNum){
+//    userNum = prompt ("You entered the wrong number, enter again");
+
+// }
+// console.log("You entered the correct number");
+
+let str = "ApnaCOllege";
+// str.length <-- Properties
+console.log(str[0]);  // <-- Methods
